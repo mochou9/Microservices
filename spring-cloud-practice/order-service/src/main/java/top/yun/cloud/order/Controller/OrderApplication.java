d5ba570b4ec1ded3a824ad02c6c6dd7027640ce9
hwkkk <376703830@qq.com>
2024-09-11 13:17:38 +0800

    2024.09.11

---
+++ spring-cloud-practice/order-service/src/main/java/top/yun/cloud/order/Controller/OrderApplication.java
@@ -0,0 +1,16 @@
+package top.yun.cloud.order.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yun
+ * @Date: 2024/09/11/ 下午1:12
+ * @description
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
